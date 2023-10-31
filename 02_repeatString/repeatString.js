a7269d9bcc0e071b6453a03e2e8a355846c3d2b0
Seven719 <ilie.andronie@gmail.com>
2023-10-31 11:52:40 +0000

    completed second exercise with repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+  if (times < 0) return "ERROR";
+  let fullString = "";
+  for (let i = 0; i < times; i++) {
+    fullString += string;
+  }
+  return fullString;
 };
 
 // Do not edit below this line
