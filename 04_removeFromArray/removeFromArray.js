ca75db6166fa9ad90ed743b45ee87a539fd1d0b1
Seven719 <ilie.andronie@gmail.com>
2023-10-31 12:32:30 +0000

    finished exercise number 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  const newArray = [];
+  array.forEach((value) => {
+    if (!args.includes(value)){
+      newArray.push(value);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
