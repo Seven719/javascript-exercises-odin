51972b6011b9b10ed765656d03c9f5638ce319ae
Seven719 <ilie.andronie@gmail.com>
2023-11-11 09:12:47 +0000

    Finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let max = str.length - 1;
+  for (let i = Math.floor(max / 2); i >= 0; i-- ) {
+    if (str[i] != str[max - i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
