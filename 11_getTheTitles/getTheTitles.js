9937ea9d9e56d4bf2315e063f6f80c2bfb2b6813
Seven719 <ilie.andronie@gmail.com>
2023-11-11 10:06:57 +0000

    Finished 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  let books = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    for (let b in array[i]) {
+      if (b == "title") {
+        books.push(array[i][b]);
+      }
+    }
+  }
+  return books;
 };
 
 // Do not edit below this line
