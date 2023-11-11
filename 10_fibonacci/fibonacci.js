8a0e989c2025c5482499e3d99b9bac5a287c2b96
Seven719 <ilie.andronie@gmail.com>
2023-11-11 09:37:00 +0000

    Finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+  if (member < 0) return 'OOPS';
 
+  let previousMember = 0;
+  let currentMember = 1;
+  
+  for (let i = 2; i <= member; i++) {
+    let temp = currentMember;
+    currentMember += previousMember;
+    previousMember = temp;
+  }
+  return currentMember;
 };
 
 // Do not edit below this line
