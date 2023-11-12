7ee52b054810ee6581532058a6ec5e41d3fe0895
Seven719 <ilie.andronie@gmail.com>
2023-11-12 10:34:57 +0000

    Finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  let oldestAge = 0, oldest;
+  
+  for(let i = 0; i < array.length; i++) {
+    let currentPerson = array[i];
+    let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    if(currentAge > oldestAge) {
+      oldest = currentPerson;
+      oldestAge = currentAge;
+    }
+  }
+  return oldest;
 };
+
+function getAge(birth, death) {
+  if(!death) {
+    return new Date().getFullYear() - birth;
+  }
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
